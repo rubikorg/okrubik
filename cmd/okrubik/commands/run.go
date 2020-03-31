29df0e5cb3cacb83a3539acb8cf001ded5067421
codekid <ckrackerx15@gmail.com>
2020-04-01 03:32:55 +0530

    done with run command with watcher implemented (fix #3)

---
+++ cmd/okrubik/commands/run.go
@@ -0,0 +1,116 @@
+package commands
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/radovskyb/watcher"
+	"github.com/rubikorg/rubik/pkg"
+)
+
+const (
+	sep = string(os.PathSeparator)
+)
+
+var cmd *exec.Cmd
+var basePath string
+
+// Run is a function for running an app from the rubik.toml file
+func Run() error {
+	pwd, _ := os.Getwd()
+	cfg := pkg.GetRubikConfig()
+	if cfg.ProjectName == "" {
+		return errors.New("not a valid rubik config")
+	}
+
+	w := watcher.New()
+	w.SetMaxEvents(1)
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt)
+	go func() {
+		for range done {
+			fmt.Println("Shutting down rubik server...")
+			killServer()
+			fmt.Println("Done!")
+			os.Exit(0)
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case _, ok := <-w.Event:
+				if !ok {
+					return
+				}
+				pkg.DebugMsg("Restarting rubik server")
+				killServer()
+				go runServer(basePath)
+			case err, ok := <-w.Error:
+				if !ok {
+					fmt.Println("Something went wrong")
+					return
+				}
+				fmt.Println(err.Error())
+			}
+
+		}
+	}()
+
+	var lookup = make(map[string]pkg.Project)
+	var options = []string{}
+	var answer string
+	for _, a := range cfg.App {
+		lookup[a.Name] = a
+		options = append(options, a.Name)
+	}
+
+	prompt := &survey.Select{
+		Message: "Select app:",
+		Options: options,
+	}
+
+	survey.AskOne(prompt, &answer)
+
+	basePath = strings.Replace(lookup[answer].Path, "./", pwd+sep, 1)
+
+	go runServer(basePath)
+
+	err := w.AddRecursive(basePath)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := w.Start(time.Millisecond * 100); err != nil {
+		log.Fatalln(err)
+	}
+	return nil
+}
+
+func runServer(basePath string) {
+	// fmt.Println("Setting new commnd", cmd.Process.Pid)
+	os.Chdir(basePath)
+	cmd = exec.Command("go", "run", basePath+sep+"main.go")
+	cmd.Stdout = os.Stdout
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setpgid: true,
+	}
+	cmd.Run()
+}
+
+func killServer() {
+	pgid, err := syscall.Getpgid(cmd.Process.Pid)
+	if err != nil {
+		fmt.Println("Cannot kill process", err.Error())
+	}
+	syscall.Kill(-pgid, syscall.SIGINT)
+}
