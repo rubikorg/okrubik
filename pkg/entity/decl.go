b783fa3351e973f709a6a425fb9c14b17b02639b
codekid <ckrackerx15@gmail.com>
2020-04-29 09:03:27 +0530

    requestentity is now entity

--- pkg/entity/decl.go
+++ pkg/entity/decl.go
@@ -7,10 +7,12 @@ import (
 // CreateBoilerplateEntity is used to get all the contents of files required for
 // `okrubik create` depending upon the data given by the user
 type CreateBoilerplateEntity struct {
-	rubik.RequestEntity
-	ModulePath string `rubik:"modulePath|query"`
-	Port       int    `rubik:"port|query"`
-	Name       string `rubik:"name|query"`
+	rubik.Entity
+	ModulePath string
+	Port       string
+	Name       string
+	Done       bool
+}
 
 // GenRouterEntity is used when user executes `okrubik gen router [name]`
 type GenRouterEntity struct {
